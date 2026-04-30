1bcfaaf3e104c4198bbc58e3e780cf3154567823
Maximilian Fischer <max@maxfs.de>
2026-04-30 17:35:13 +0200

    refactor: move qr code generation to viewModel

--- PayForMe/Views/Projects/ShareProjectQRCode.swift
+++ PayForMe/Views/Projects/ShareProjectQRCode.swift
@@ -6,49 +6,27 @@
 //
 
 import SwiftUI
-import CoreImage.CIFilterBuiltins
-import UIKit
 
 struct ShareProjectQRCode: View {
-    let project: Project
 
-    private static let context = CIContext()
+    @StateObject private var viewModel: ShareProjectQRCodeViewModel
 
-    private var dataString: String {
-        let server = project.url.relativeString
-            .replacingOccurrences(of: "https://", with: "")
-        return "cospend://\(server)/\(project.token)/\(project.password)"
-    }
-
-    private var qrCodeImage: UIImage {
-        generateQRCode(from: dataString)
+    init(project: Project) {
+        _viewModel = StateObject(wrappedValue: ShareProjectQRCodeViewModel(project: project))
     }
 
     var body: some View {
         VStack {
-            Text(dataString).font(.caption)
+            Text(viewModel.dataString).font(.caption)
 
-            Image(uiImage: qrCodeImage)
-                .interpolation(.none)
-                .resizable()
-                .scaledToFit()
+            if let image = viewModel.qrCodeImage {
+                Image(uiImage: image)
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+            }
         }
         .padding()
-    }
-
-    private func generateQRCode(from string: String) -> UIImage {
-        let filter = CIFilter.qrCodeGenerator()
-        filter.message = Data(string.utf8)
-
-        guard let outputImage = filter.outputImage else {
-            return UIImage(systemName: "xmark.circle") ?? UIImage()
-        }
-
-        guard let cgImage = Self.context.createCGImage(outputImage, from: outputImage.extent) else {
-            return UIImage(systemName: "xmark.circle") ?? UIImage()
-        }
-
-        return UIImage(cgImage: cgImage)
     }
 }
 
